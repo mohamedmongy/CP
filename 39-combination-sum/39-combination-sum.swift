b2cac65904911b6888d08afd057f30f90859cb8a
mohamed mongy <31741978+mohamedmongy@users.noreply.github.com>
2022-05-15 21:02:34 +0200

    Time: 14 ms (83.95%), Space: 13.8 MB (95.26%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.swift
@@ -0,0 +1,37 @@
+class Solution {
+   func combinationSum(_ candidates: [Int], _ target: Int) -> [[Int]] {
+    
+    func dfs(input: inout [Int], idx: Int, output: inout [[Int]], total: Int, target: Int ) {
+        
+        if total == target {
+            output.append(input)
+            return
+        }
+        
+        if total > target {
+            return
+        }
+        
+        if idx >= candidates.count {
+            return
+        }
+        
+        // take the elment
+        input.append(candidates[idx])
+        dfs(input: &input, idx: idx, output: &output, total:  total + candidates[idx], target: target)
+        
+        // don't take the element
+        input.removeLast()
+        dfs(input: &input, idx: idx+1, output: &output, total: total, target: target)
+        
+    }
+    
+    var res: [[Int]] = []
+    var input: [Int] = []
+    
+    dfs(input: &input, idx: 0, output: &res, total: 0, target: target)
+    
+    return res
+}
+  
+}
