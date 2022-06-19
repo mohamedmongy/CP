d2302b4b27839f4eeb942808bb30ba3b0d32f0ec
mohamed mongy <31741978+mohamedmongy@users.noreply.github.com>
2022-06-19 10:52:29 +0200

    Time: 62 ms (47.22%), Space: 15.4 MB (80.51%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.py
@@ -4,22 +4,55 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+import collections
+
 class Solution:
     
-      def dfs(self, node: TreeNode):
-        if not node: 
-            return 0 
-        
-        left = self.dfs(node.left)
-        right = self.dfs( node.right)
-        
-        return  1 + max(left, right)
     
-      def maxDepth(self, root: Optional[TreeNode]) -> int:
-          if not root:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
              return 0 
+            
+            
+        return self.bfs(root)   
+         # return 1 + max(self.dfs(root.left), self.dfs(root.right))
         
-          return 1 + max(self.dfs(root.left), self.dfs(root.right))
+        
+    def bfs(self, root: TreeNode) -> int :
+            res = 0 
+            q =  deque( [root])
+            
+            while q:
+                for i in range(len(q)):
+                        node = q.popleft() 
+                        if node.left: 
+                            q.append( node.left)
+                        if node.right: 
+                            q.append( node.right)
+
+                res += 1   
+            return res 
+                        
+    
+    
+    
+    
+      #def dfs(self, node: TreeNode):
+       # if not node: 
+        #    return 0 
+        
+        #left = self.dfs(node.left)
+        #right = self.dfs( node.right)
+        
+        #return  1 + max(left, right)
+    
+    
+    
+        
+       
+        
+ 
             
   
         
