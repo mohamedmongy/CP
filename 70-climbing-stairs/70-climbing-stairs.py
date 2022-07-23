7c0671919af0532b854e4dd47fdce6aff3c83e8d
mohamed mongy <31741978+mohamedmongy@users.noreply.github.com>
2022-07-24 00:10:01 +0200

    Time: 69 ms (5.76%), Space: 13.9 MB (56.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,10 +2,10 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         prev , prev2 = 1, 1
         
-        for i in range(n-1):
-            t = prev 
-            prev = prev + prev2 
-            prev2 = t 
+        for i in range(2,n+1):
+            c = prev + prev2 
+            prev2 = prev 
+            prev = c
          
         return prev 
         
